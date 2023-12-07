b7ef33d4f63196733a43570ad88a9ee770415e96
pidgay <kacper.goch.pasp@gmail.com>
2023-12-07 21:28:03 +0100

    Added features

--- src/controller/BackupService.java
+++ src/controller/BackupService.java
@@ -2,8 +2,29 @@ package controller;
 
 public class BackupService {
     public void start(boolean flagOperation,boolean flagCompressionType, String fileName){
-
+        if(flagOperation){
+            backup(flagCompressionType, fileName);
+        }
+        else{
+            restore(flagCompressionType, fileName);
+        }
     }
 
+    private void backup(boolean flagCompressionType, String fileName){
+        if (flagCompressionType){
 
+        }
+        else{
+
+        }
+    }
+
+    private void restore(boolean flagCompressionType, String fileName){
+        if (flagCompressionType){
+
+        }
+        else{
+
+        }
+    }
 }
